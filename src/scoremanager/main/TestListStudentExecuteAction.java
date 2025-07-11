9b0d0cb98e1674d46c08fe28fd95e35482b6711c
spr2440151 <spr2440151@stu.o-hara.ac.jp>
2025-07-11 12:38:49 +0900

    7/11 学生検索 一部

--- src/scoremanager/main/TestListStudentExecuteAction.java
+++ src/scoremanager/main/TestListStudentExecuteAction.java
@@ -10,22 +10,25 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import bean.School;
+import bean.Student;
 import bean.Subject;
 import bean.Teacher;
-import bean.TestListSubject;
+import bean.TestListStudent;
 import dao.ClassNumDao;
+import dao.StudentDao;
 import dao.SubjectDao;
-import dao.TestListSubjectDao;
+import dao.TestListStudentDao;
 import tool.Action;
 
-public class TestListSubjectExecuteAction extends Action {
+public class TestListStudentExecuteAction extends Action {
     public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
         try {
             HttpSession session = req.getSession();
             SubjectDao sdao = new SubjectDao();
             ClassNumDao cdao = new ClassNumDao();
+            StudentDao studao = new StudentDao();
 
-            TestListSubjectDao subldao = new TestListSubjectDao();
+            TestListStudentDao stuldao = new TestListStudentDao();
             Teacher teacher = (Teacher) session.getAttribute("user");
             School school = teacher.getSchool();
 
@@ -33,10 +36,12 @@ public class TestListSubjectExecuteAction extends Action {
             String entyStr = req.getParameter("f1");
             String classNum = req.getParameter("f2");
             String subCd = req.getParameter("f3");
+            String no = req.getParameter("f4");
 
             System.out.println("[DEBUG]入学:" + entyStr);
             System.out.println("[DEBUG]クラス:" + classNum);
             System.out.println("[DEBUG]科目:" + subCd);
+            System.out.println("[DEBUG]学番:" + no);
 
             // 入学年度リスト生成
             List<Integer> yearList = new ArrayList<>();
@@ -52,20 +57,15 @@ public class TestListSubjectExecuteAction extends Action {
             List<String> cList = cdao.filter(school);
 
             // 検索条件がある場合のみテスト一覧を取得
-            if (entyStr != null && !entyStr.isEmpty() &&
-                classNum != null && !classNum.isEmpty() &&
-                subCd != null && !subCd.isEmpty()) {
+            if (no != null && !no.isEmpty()) {
 
             	System.out.println("if文チェック");
-                int enty = Integer.parseInt(entyStr);
-                Subject sub = sdao.get(subCd, school);
+                Student stu = studao.get(no);
 
-                List<TestListSubject> sublist =  subldao.filter(enty, classNum, sub, school);
-                session.setAttribute("enty", enty);
+                List<TestListStudent> stulist =  stuldao.filter(stu);
                 session.setAttribute("classnum", classNum);
-                session.setAttribute("subt", sub);
                 session.setAttribute("school", school);
-                session.setAttribute("subList", sublist);
+                session.setAttribute("stuList", stulist);
             }
 
             System.out.println("[DEBUG]s:" + sList);
